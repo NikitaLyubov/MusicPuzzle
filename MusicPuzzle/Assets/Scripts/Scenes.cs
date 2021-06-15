51ff676f9cf1d7d0d32c423c088ec4555085c987
Никита Любов <nikita_lyubov666@mail.ru>
2021-06-15 21:46:17 +0500

    Update

--- MusicPuzzle/Assets/Scripts/Scenes.cs
+++ MusicPuzzle/Assets/Scripts/Scenes.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class Scenes : MonoBehaviour
 {
+    private int previousScene;
+
     public void NextScene(int sceneIndex)
     {
+        previousScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void PreviousScene()
+    {
+        SceneManager.LoadScene(previousScene);
     }
 }
---
+++ MusicPuzzle/Assets/Scripts/VolumeChganger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeChganger : MonoBehaviour
+{
+    private float musicVolume = 1f;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        AudioListener.volume = musicVolume;
+    }
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+
+
+}
